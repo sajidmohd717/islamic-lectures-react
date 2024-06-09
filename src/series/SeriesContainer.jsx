e76d55a5de5388a0771d0cda3663fda4fa24e4b6
Sajid <sajidmohammad460519@gmail.com>
2024-06-09 13:05:31 +0800

    added new series to heart softeners, kinda sick of front end already, like i still have so much to do for this project, but damn

--- src/series/SeriesContainer.jsx
+++ src/series/SeriesContainer.jsx
@@ -4,12 +4,14 @@ import styles from "./SeriesContainer.module.css";
 
 import whyMe from "../assets/thumbnail/heart-softeners/whyme.jpg";
 import angels1 from "../assets/thumbnail/heart-softeners/angels1.jpg";
+import changeofheart from '../assets/thumbnail/heart-softeners/changeofheart.jpg'
 
 function SeriesDraft() {
   let whymewebsite =
     "https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa";
   let angelswebsite =
     "https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa";
+  
 
   return (
     <>
@@ -23,6 +25,13 @@ function SeriesDraft() {
         />
         <Series
           thumbnail={angels1}
+          link={angelswebsite}
+          title={"Angels in Your Presence"}
+          speaker={"Omar Suleiman"}
+          episodes={"32 Episodes"}
+        />
+        <Series
+          thumbnail={changeofheart}
           link={angelswebsite}
           title={"Angels in Your Presence"}
           speaker={"Omar Suleiman"}
