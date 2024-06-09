653c3e3e76ffb480365e144d1626de0aa0a35568
Sajid <sajidmohammad460519@gmail.com>
2024-06-09 09:01:09 +0800

    wouah, now the series component is super dynamic, can add more serieses easily, but still needs more work

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Speakers from "./speaker/Speakers.jsx";
 import Series from "./series/Series.jsx"
+import SeriesDraft from "./series/SeriesDraft.jsx";
+
 
 function App() {
   return (
@@ -10,7 +12,7 @@ function App() {
       <Speakers />
       <hr />
       <h3>Heart Softeners</h3>
-      <Series />
+      <SeriesDraft />
       <Footer />
     </>
   );
--- src/series/Series.jsx
+++ src/series/Series.jsx
@@ -3,21 +3,21 @@ import downArrow from "../assets/down-arrow.png";
 
 import styles from "./Series.module.css";
 
-function Series() {
+function Series(props) {
   return (
-    <a href="https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa">
+    <a href={props.link}>
       <div className={styles["series-container"]}>
         <div className="image-container">
-          <img className={styles["thumbnail-img"]} src={whyMe} alt="" />
+          <img className={styles["thumbnail-img"]} src={props.thumbnail} alt="" />
         </div>
         <div className={styles["text-container"]}>
           <div className={styles["title"]}>
-            <span>Why Me | 2024 Ramadan Series</span>
+            <span>{props.title}</span>
           </div>
           <div className={styles["meta-title"]}>
             <div className={styles["meta-title-left"]}>
-              <span>Omar Suleiman</span>
-              <span>32 Episodes</span>
+              <span>{props.speaker}</span>
+              <span>{props.episodes}</span>
             </div>
             <div className={styles["meta-title-right"]}>
               <img className={styles["down-arrow"]} src={downArrow} alt="" />
