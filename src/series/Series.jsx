5cbd91eab012c50306d398c3798ce4be6e77a077
Sajid <sajidmohammad460519@gmail.com>
2024-06-09 08:42:36 +0800

    improved series component, made whole component and anchor tag and improved the color of the links

--- src/series/Series.jsx
+++ src/series/Series.jsx
@@ -5,27 +5,27 @@ import styles from "./Series.module.css";
 
 function Series() {
   return (
-    <div className={styles["series-container"]}>
-      <div className="image-container">
-        <a href="https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa">
+    <a href="https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa">
+      <div className={styles["series-container"]}>
+        <div className="image-container">
           <img className={styles["thumbnail-img"]} src={whyMe} alt="" />
-        </a>
-      </div>
-      <div className={styles["text-container"]}>
-        <div className={styles["title"]}>
-          <span>Why Me | 2024 Ramadan Series</span>
         </div>
-        <div className={styles["meta-title"]}>
-          <div className={styles["meta-title-left"]}>
-            <span>Omar Suleiman</span>
-            <span>32 Episodes</span>
+        <div className={styles["text-container"]}>
+          <div className={styles["title"]}>
+            <span>Why Me | 2024 Ramadan Series</span>
           </div>
-          <div className={styles["meta-title-right"]}>
-            <img className={styles["down-arrow"]} src={downArrow} alt="" />
+          <div className={styles["meta-title"]}>
+            <div className={styles["meta-title-left"]}>
+              <span>Omar Suleiman</span>
+              <span>32 Episodes</span>
+            </div>
+            <div className={styles["meta-title-right"]}>
+              <img className={styles["down-arrow"]} src={downArrow} alt="" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </a>
   );
 }
 
