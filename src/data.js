5f40976c150af842b4c5c3f9eaf31f7dc7d6bd27
Sajid <sajidmohammad460519@gmail.com>
2024-06-09 19:57:52 +0800

    added 1 more video to heart softeners section

--- src/data.js
+++ src/data.js
@@ -1,6 +1,7 @@
 import whyMe from "./assets/thumbnail/heart-softeners/whyme.jpg";
 import angels1 from "./assets/thumbnail/heart-softeners/angels1.jpg";
 import changeofheart from "./assets/thumbnail/heart-softeners/changeofheart.jpg";
+import heartmatters from "./assets/thumbnail/heart-softeners/heartmatters.jpg";
 
 export const heartSoftenersDict = [
   {
@@ -9,7 +10,7 @@ export const heartSoftenersDict = [
     episodes: "32 Episodes",
     thumbnailImage: whyMe,
     link: "https://www.youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa",
-    viewcount: "14.3 Mil"
+    viewcount: "14.3 Mil",
   },
   {
     title: "Angels in Your Presence",
@@ -20,9 +21,16 @@ export const heartSoftenersDict = [
   },
   {
     title: "Change of Heart",
-    speaker: "Omar Suleiman",
-    episodes: "32 Episodes",
+    speaker: "Ali Hammuda",
+    episodes: "12 Episodes",
     thumbnailImage: changeofheart,
     link: "https://www.youtube.com/playlist?list=PL9OPVukugS7xZ-PY008PN6_kGInouP0rz",
+  },
+  {
+    title: "Heart Matters Ramadan Series 2023",
+    speaker: "Yasir Qadhi",
+    episodes: "27 Episodes",
+    thumbnailImage: heartmatters,
+    link: "https://www.youtube.com/playlist?list=PLYZxc42QNctWeXvciIWtItbjhod9PjcCN",
   },
 ];
