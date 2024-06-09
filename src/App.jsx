1fbdb7c8326496ddc90421b73ffa003bbedb21ad
Sajid <sajidmohammad460519@gmail.com>
2024-06-09 14:07:27 +0800

    now my code's even more dynamic, added a data.js file to import the dictionary and images from

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,9 @@ import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Speakers from "./speaker/Speakers.jsx";
 import SeriesContainer from "./series/SeriesContainer.jsx";
-
-import whyMe from "./assets/thumbnail/heart-softeners/whyme.jpg";
-import angels1 from "./assets/thumbnail/heart-softeners/angels1.jpg";
-import changeofheart from './assets/thumbnail/heart-softeners/changeofheart.jpg';
+import { heartSoftenersDict } from "./data.js";
 
 function App() {
-  let heartSoftenersDict = [
-    {
-      title: "Why Me | 2024 Ramadan Series",
-      speaker: "Omar Suleiman",
-      episodes: "32 Episodes",
-      thumbnailImage: whyMe,
-      link: "https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa"
-    },
-    {
-      title: "Angels in Your Presence",
-      speaker: "Omar Suleiman",
-      episodes: "32 Episodes",
-      thumbnailImage: angels1,
-      link: "https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa"
-    },
-    {
-      title: "Change of Heart",
-      speaker: "Omar Suleiman",
-      episodes: "32 Episodes",
-      thumbnailImage: changeofheart,
-      link: "https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa"
-    }
-  ];
-
   return (
     <>
       <Header />
---
+++ src/data.js
@@ -0,0 +1,27 @@
+import whyMe from "./assets/thumbnail/heart-softeners/whyme.jpg";
+import angels1 from "./assets/thumbnail/heart-softeners/angels1.jpg";
+import changeofheart from "./assets/thumbnail/heart-softeners/changeofheart.jpg";
+
+export const heartSoftenersDict = [
+  {
+    title: "Why Me | 2024 Ramadan Series",
+    speaker: "Omar Suleiman",
+    episodes: "32 Episodes",
+    thumbnailImage: whyMe,
+    link: "https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa",
+  },
+  {
+    title: "Angels in Your Presence",
+    speaker: "Omar Suleiman",
+    episodes: "32 Episodes",
+    thumbnailImage: angels1,
+    link: "https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa",
+  },
+  {
+    title: "Change of Heart",
+    speaker: "Omar Suleiman",
+    episodes: "32 Episodes",
+    thumbnailImage: changeofheart,
+    link: "https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa",
+  },
+];
