80c52551d88d6ee3c60eab2e1e3e5a114e9eb1ef
Sajid <sajidmohammad460519@gmail.com>
2024-06-09 14:03:35 +0800

    wow, very proud of my code, thanks chatgpt, super dynamic now, and very robust indeed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,48 @@
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Speakers from "./speaker/Speakers.jsx";
-import Series from "./series/Series.jsx"
-import SeriesDraft from "./series/SeriesContainer.jsx";
+import SeriesContainer from "./series/SeriesContainer.jsx";
 
+import whyMe from "./assets/thumbnail/heart-softeners/whyme.jpg";
+import angels1 from "./assets/thumbnail/heart-softeners/angels1.jpg";
+import changeofheart from './assets/thumbnail/heart-softeners/changeofheart.jpg';
 
 function App() {
+  let heartSoftenersDict = [
+    {
+      title: "Why Me | 2024 Ramadan Series",
+      speaker: "Omar Suleiman",
+      episodes: "32 Episodes",
+      thumbnailImage: whyMe,
+      link: "https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa"
+    },
+    {
+      title: "Angels in Your Presence",
+      speaker: "Omar Suleiman",
+      episodes: "32 Episodes",
+      thumbnailImage: angels1,
+      link: "https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa"
+    },
+    {
+      title: "Change of Heart",
+      speaker: "Omar Suleiman",
+      episodes: "32 Episodes",
+      thumbnailImage: changeofheart,
+      link: "https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa"
+    }
+  ];
+
   return (
     <>
       <Header />
       <Speakers />
       <hr />
-      <h3>Heart Softeners</h3>
-      <SeriesDraft />
+      <SeriesContainer
+        sectionHead={"Heart Softeners"}
+        listOfDict={heartSoftenersDict}
+      />
+      <hr />
+      <SeriesContainer sectionHead={"General Quran Tafsir"} listOfDict={[]} />
       <Footer />
     </>
   );
--- src/series/SeriesContainer.jsx
+++ src/series/SeriesContainer.jsx
@@ -1,45 +1,27 @@
 import Series from "./Series.jsx";
-
 import styles from "./SeriesContainer.module.css";
 
-import whyMe from "../assets/thumbnail/heart-softeners/whyme.jpg";
-import angels1 from "../assets/thumbnail/heart-softeners/angels1.jpg";
-import changeofheart from '../assets/thumbnail/heart-softeners/changeofheart.jpg'
-
-function SeriesDraft() {
-  let whymewebsite =
-    "https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa";
-  let angelswebsite =
-    "https://youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa";
-  
-
+function SeriesContainer(props) {
   return (
     <>
+      <div className={styles["section-heading"]}>
+        <h3 className={styles["section-title"]}>{props.sectionHead}</h3>
+        <button className={styles['view-all-btn']}>View all</button>
+      </div>
       <div className={styles['series-container']}>
-        <Series
-          thumbnail={whyMe}
-          link={whymewebsite}
-          title={"Why Me | 2024 Ramadan Series"}
-          speaker={"Omar Suleiman"}
-          episodes={"32 Episodes"}
-        />
-        <Series
-          thumbnail={angels1}
-          link={angelswebsite}
-          title={"Angels in Your Presence"}
-          speaker={"Omar Suleiman"}
-          episodes={"32 Episodes"}
-        />
-        <Series
-          thumbnail={changeofheart}
-          link={angelswebsite}
-          title={"Angels in Your Presence"}
-          speaker={"Omar Suleiman"}
-          episodes={"32 Episodes"}
-        />
+        {props.listOfDict.map((series, index) => (
+          <Series
+            key={index}
+            thumbnail={series.thumbnailImage}
+            link={series.link}
+            title={series.title}
+            speaker={series.speaker}
+            episodes={series.episodes}
+          />
+        ))}
       </div>
     </>
   );
 }
 
-export default SeriesDraft;
+export default SeriesContainer;
