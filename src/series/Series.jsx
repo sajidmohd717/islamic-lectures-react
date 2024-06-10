5991e66fbcde62a026c0e78acf526e44191a2f97
Sajid <sajidmohammad460519@gmail.com>
2024-06-10 18:47:41 +0800

    handleclick event for down arrow, 1% progress

--- src/series/Series.jsx
+++ src/series/Series.jsx
@@ -4,26 +4,44 @@ import downArrow from "../assets/down-arrow.png";
 import styles from "./Series.module.css";
 
 function Series(props) {
+  /* The plan is to show like a short description of what the 
+               series is about when the down arrow is clicked
+  */
+  const handleclick = (e) => {
+    console.log("down arrow has been clicked");
+  };
+
   return (
-      <div className={styles["series-container"]}>
-        <div className="image-container">
-          <a href={props.link} target="_blank"><img className={styles["thumbnail-img"]} src={props.thumbnail} alt="" /></a>
+    <div className={styles["series-container"]}>
+      <div className="image-container">
+        <a href={props.link} target="_blank">
+          <img
+            className={styles["thumbnail-img"]}
+            src={props.thumbnail}
+            alt=""
+          />
+        </a>
+      </div>
+      <div className={styles["text-container"]}>
+        <div className={styles["title"]}>
+          <span>{props.title}</span>
         </div>
-        <div className={styles["text-container"]}>
-          <div className={styles["title"]}>
-            <span>{props.title}</span>
+        <div className={styles["meta-title"]}>
+          <div className={styles["meta-title-left"]}>
+            <span>{props.speaker}</span>
+            <span>{props.episodes}</span>
           </div>
-          <div className={styles["meta-title"]}>
-            <div className={styles["meta-title-left"]}>
-              <span>{props.speaker}</span>
-              <span>{props.episodes}</span>
-            </div>
-            <div className={styles["meta-title-right"]}>
-              <img className={styles["down-arrow"]} src={downArrow} alt="" />
-            </div>
+          <div className={styles["meta-title-right"]}>
+            <img
+              onClick={(e) => handleclick(e)}
+              className={styles["down-arrow"]}
+              src={downArrow}
+              alt=""
+            />
           </div>
         </div>
       </div>
+    </div>
   );
 }
 
@@ -34,6 +52,6 @@ Series.defaultProps = {
   thumbnailImage: "default image",
   link: "default link",
   viewcount: "default viewcount",
-}
+};
 
 export default Series;
