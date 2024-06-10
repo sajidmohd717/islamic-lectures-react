994c9f3c7b66bf544ced78bb9a9c21a802472411
Sajid <sajidmohammad460519@gmail.com>
2024-06-10 19:34:19 +0800

    managed to get some functionality into the down arrow button, but styling can be improved

--- src/series/Series.jsx
+++ src/series/Series.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import whyMe from "../assets/thumbnail/heart-softeners/whyme.jpg";
 import downArrow from "../assets/down-arrow.png";
 
@@ -7,41 +9,58 @@ function Series(props) {
   /* The plan is to show like a short description of what the 
                series is about when the down arrow is clicked
   */
-  const handleclick = (e) => {
-    console.log("down arrow has been clicked");
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
   };
 
+  console.log("down arrow has been clicked");
+
   return (
-    <div className={styles["series-container"]}>
-      <div className="image-container">
-        <a href={props.link} target="_blank">
-          <img
-            className={styles["thumbnail-img"]}
-            src={props.thumbnail}
-            alt=""
-          />
-        </a>
-      </div>
-      <div className={styles["text-container"]}>
-        <div className={styles["title"]}>
-          <span>{props.title}</span>
-        </div>
-        <div className={styles["meta-title"]}>
-          <div className={styles["meta-title-left"]}>
-            <span>{props.speaker}</span>
-            <span>{props.episodes}</span>
+    <>
+      <div className={styles["series-with-description"]}>
+        <div className={styles["series-container"]}>
+          <div className="image-container">
+            <a href={props.link} target="_blank">
+              <img
+                className={styles["thumbnail-img"]}
+                src={props.thumbnail}
+                alt=""
+              />
+            </a>
           </div>
-          <div className={styles["meta-title-right"]}>
-            <img
-              onClick={(e) => handleclick(e)}
-              className={styles["down-arrow"]}
-              src={downArrow}
-              alt=""
-            />
+          <div className={styles["text-container"]}>
+            <div className={styles["title"]}>
+              <span>{props.title}</span>
+            </div>
+            <div className={styles["meta-title"]}>
+              <div className={styles["meta-title-left"]}>
+                <span>{props.speaker}</span>
+                <span>{props.episodes}</span>
+              </div>
+              <div className={styles["meta-title-right"]}>
+                <img
+                  onClick={toggleVisibility}
+                  className={styles["down-arrow"]}
+                  src={downArrow}
+                  alt=""
+                />
+              </div>
+            </div>
           </div>
         </div>
+        <div className="description-container">
+          {isVisible && (
+            <div className="items">
+              <p>Item 1</p>
+              <p>Item 2</p>
+              <p>Item 3</p>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
