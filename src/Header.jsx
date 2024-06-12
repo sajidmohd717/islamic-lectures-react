a0c963234c88bc31ed1c7f20f7b053f0bee7c4f3
Sajid <sajidmohammad460519@gmail.com>
2024-06-13 07:14:29 +0800

    added catagories tab on the top of website like youtube has

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,22 @@
-import hamburger from "./assets/hamburger.webp"
+import hamburger from "./assets/hamburger.webp";
 
 function Header() {
   return (
     <header>
-      <p className="nav-info">Welcome to islamicseries.org</p>
-      <img className="hamburger" src={hamburger} alt="" />
+      <div className="top-header">
+        <p className="nav-info">Welcome to islamicseries.org</p>
+        <img className="hamburger" src={hamburger} alt="" />
+      </div>
+      <div className="catagories-container">
+        <div className="catagories">
+          <button className="cat-btn">All</button>
+          <button className="cat-btn">Heart</button>
+          <button className="cat-btn">Reflection</button>
+          <button className="cat-btn">Quran Tafsir</button>
+          <button></button>
+        </div>
+        <div className="view-all-cat"></div>
+      </div>
     </header>
   );
 }
