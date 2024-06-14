b09aa9c9311f288596bed92a0e4b14c18ba0b489
Sajid Mohamamd <89695946+sajidmohd717@users.noreply.github.com>
2024-06-14 12:40:08 +0800

    Update App.jsx
    Updated word heading

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import {
 function App() {
   let seriesDict = [
     {
-      sectionHead: "Curing the Heart",
+      sectionHead: "Purification of the Heart",
       listOfDict: curingHeartDict
     },
     {
