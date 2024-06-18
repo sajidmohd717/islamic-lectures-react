1fca235ba111b618bd48a63faf0f7d0bba20fe3e
Sajid Mohamamd <89695946+sajidmohd717@users.noreply.github.com>
2024-06-18 14:30:03 +0800

    Update data.js
    Changed episodes to lectures

--- src/data/data.js
+++ src/data/data.js
@@ -17,7 +17,7 @@ export const curingHeartDict = [
   {
     title: "Change of Heart",
     speaker: "Ali Hammuda",
-    episodes: "12 Episodes",
+    episodes: "12 Lectures",
     thumbnailImage: changeofheart,
     link: "https://www.youtube.com/playlist?list=PL9OPVukugS7xZ-PY008PN6_kGInouP0rz",
     viewcount: "628K views" //Updated 12 June 2024
@@ -25,7 +25,7 @@ export const curingHeartDict = [
   {
     title: "Heart Matters Ramadan Series 2023",
     speaker: "Yasir Qadhi",
-    episodes: "27 Episodes",
+    episodes: "27 Lectures",
     thumbnailImage: heartmatters,
     link: "https://www.youtube.com/playlist?list=PLYZxc42QNctWeXvciIWtItbjhod9PjcCN",
     viewcount: "749K views" //Updated 12 June 2024
@@ -36,7 +36,7 @@ export const reflectionContemplationDict = [
   {
     title: "Why Me | 2024 Ramadan Series",
     speaker: "Omar Suleiman",
-    episodes: "32 Episodes",
+    episodes: "32 Lectures",
     thumbnailImage: whyMe,
     link: "https://www.youtube.com/playlist?list=PLQ02IYL5pmhFYDrmxNHAlwgcHOR4h1bPa",
     viewcount: "14.3 Mil",
@@ -44,7 +44,7 @@ export const reflectionContemplationDict = [
   {
     title: "Angels in Your Presence",
     speaker: "Omar Suleiman",
-    episodes: "32 Episodes",
+    episodes: "32 Lectures",
     thumbnailImage: angels1,
     link: "https://www.youtube.com/playlist?list=PLQ02IYL5pmhF2LFN-3QxnuregEv1oKPIc",
   },
@@ -55,21 +55,21 @@ export const generalQuranTafsir = [
   {
     title: "The Message of the Quran in 30 Lessons",
     speaker: "Yasir Qadhi",
-    episodes: "30 Episodes",
+    episodes: "30 Lectures",
     thumbnailImage: messageQuran,
     link: "https://www.youtube.com/playlist?list=PLYZxc42QNctUnn09Of4rBuakQhu-Q2qpc",
   },
   {
     title: "The Parables of The Quran",
     speaker: "Yasir Qadhi",
-    episodes: "29 Episodes",
+    episodes: "29 Lectures",
     thumbnailImage: parablesQuran,
     link: "https://www.youtube.com/playlist?list=PLYZxc42QNctUIsBRE5XCY6eICwl_W8jnj",
   },
   {
     title: "Wisdoms of The Quran - Ramadan Series 2024",
     speaker: "Yasir Qadhi",
-    episodes: "26 Episodes",
+    episodes: "26 Lectures",
     thumbnailImage: wisdomsQuran,
     link: "https://www.youtube.com/playlist?list=PLYZxc42QNctV2v3RRYwTHdgDHp_h80mJT",
   },
@@ -79,21 +79,21 @@ export const lifeOfProphetMuhammad = [
   {
     title: "Seerah of Prophet Muhammed (S)",
     speaker: "Yasir Qadhi",
-    episodes: "104 Episodes",
+    episodes: "104 Lectures",
     thumbnailImage: seerahYasirQadhi,
     link: "https://www.youtube.com/playlist?list=PLAEA99D24CA2F9A8F"
   },
   {
     title: "Life of Prophet Muhammad (S)",
     speaker: "Uthman Ibn Farooq",
-    episodes: "35 Episodes", 
+    episodes: "35 Lectures", 
     thumbnailImage: seerahUthman,
     link: "https://www.youtube.com/playlist?list=PLlXVKBG9es9UamBMS7aubQSGe8u9RYGOk"
   },
   {
     title: "Ramadan 2012 - Life of Muhammad - PBUH",
     speaker: "Mufti Menk",
-    episodes: "29 Episodes",
+    episodes: "29 Lectures",
     thumbnailImage: seerahMufti,
     link: "https://www.youtube.com/playlist?list=PL91FA0E3B7837B214",
   }
@@ -103,7 +103,7 @@ export const angelsDict = [
   {
     title: "Angels in Your Presence",
     speaker: "Omar Suleiman",
-    episodes: "32 Episodes",
+    episodes: "32 Lectures",
     thumbnailImage: angels1,
     link: "https://www.youtube.com/playlist?list=PLQ02IYL5pmhF2LFN-3QxnuregEv1oKPIc",
   },
@@ -113,7 +113,7 @@ export const hadithDict = [
   {
     title: "Fortress Of The Muslim",
     speaker: "Assim Al-Hakeem",
-    episodes: "73 Episodes",
+    episodes: "73 Lectures",
     thumbnailImage: fortress,
     link : "https://www.youtube.com/playlist?list=PLmeZZmICk2QfIAmxPlZDEge9uwE2NO5DX"
   },
