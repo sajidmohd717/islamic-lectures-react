7ab0df593ca0ddca4675ef24d28fde6992e62259
Sajid <sajidmohammad460519@gmail.com>
2024-06-22 16:47:07 +0800

    got a pretty nice UI for the signup page, let's see how it goes

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,11 +1,35 @@
-import Nav from "../../assets/Components/navigation/Nav"
+import Nav from "../../assets/Components/navigation/Nav";
+import styles from "./Signup.module.css";
+import React, { useState } from 'react';
 
 function Signup() {
-    return(
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
         <>
-        <Nav></Nav>
-        <h1>Signup Page</h1></>
-    )
+            <Nav />
+            <div className={styles['signup-container']}>
+                <h1>Sign Up</h1>
+                <form action="example.com/path" method="post" autoComplete="off">
+                    <div>
+                        <label htmlFor="name">Name </label>
+                        <input type="text" placeholder="Full Name" id="name" value={name} onChange={(e) => setName(e.target.value)} autoComplete="off"/>
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email </label>
+                        <input type="email" placeholder="Email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} autoComplete="new-email"/>
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password </label>
+                        <input type="password" placeholder="Password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} autoComplete="new-password"/>
+                    </div>
+                    <button type="submit">Sign Up</button>
+                </form>
+            </div>
+        </>
+    );
 }
 
-export default Signup
+export default Signup;
